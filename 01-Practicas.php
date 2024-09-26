d962c76e0ecd24f0b64d8e053cd8dc8980d25de5
jose.gomez <javierrumo2@gmail.com>
2024-09-26 09:22:09 +0200

    Primer ejercicio subido

---
+++ 01-Practicas.php
@@ -0,0 +1,20 @@
+<?php
+
+// Almacena tres números en variables y escribirlos en pantalla de manera ordenada.
+$a = 10;
+$b = 20;
+$c = 6;
+
+if ($a<= $b && $a<=$c && $b<=$c) {
+    print("$a - $b - $c ");
+} elseif($a <=$b && $b>=$c){
+    print("$a - $c - $b ");
+} elseif($b<= $a && $b<$c && $a>$c){
+    print("$b - $c - $a");
+} elseif($b<= $a && $b<$c && $a<$c){
+    print("$b - $a - $c");
+} elseif($c<= $a && $c<$b && $a>$b){
+    print("$b - $a - $b");
+} elseif($c<= $a && $c<$b && $a>$b){
+    print("$c - $b - $a");
+}
